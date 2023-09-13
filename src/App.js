f9d3a4522c0695b7c3ddfec07875f12c1a926f8e
Dasom Park <dev.somda@gmail.com>
2023-09-13 13:14:32 +0900

    STYLE: 화면 권장 사이즈 표시 시점 설정

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
   return (
     <div className="App">
       <h2>{TITLE} 데이터</h2>
-      <p>
+      <p className="ui-warning-text">
         ※ 차트의 전체 내용을 확인하기 위해서는 화면의 가로 길이를 900px 이상
         유지하길 권장드립니다.
       </p>
