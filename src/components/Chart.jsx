ddac463f1c9ff7f54cf4969d41a2b5a33dd964f6
Dasom Park <dev.somda@gmail.com>
2023-09-13 12:40:08 +0900

    CHORE: 불필요한 콘솔 출력 제거

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -18,7 +18,7 @@ import CustomToolTip from "./CustomToolTip";
 import { useFilterContext } from "../context/filterContext";
 
 const prevData = objToArr(mockData.response, "date");
-// prevdata 배열을 변환하여 date 속성만 변경
+// prevdata 배열을 변환하여 date 속성만 전체 연월일시간 -> 시간으로 변경
 const data = prevData.map((prev) => ({
   ...prev,
   date: getTime(prev.date),
@@ -26,7 +26,6 @@ const data = prevData.map((prev) => ({
 
 export default function Chart() {
   const { filteredIds } = useFilterContext();
-  console.log(filteredIds);
 
   return (
     <div className="chart-container">
